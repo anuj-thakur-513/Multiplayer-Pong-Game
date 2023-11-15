b5e5f2587fc3040f8a6ddcda0ae4ff302eb868be
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-11-15 22:20:05 +0530

    connected socket.io with express

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
-const server = require("http").createServer();
-const io = require("socket.io")(server, {
+const http = require("http");
+const io = require("socket.io");
+
+const { api } = require("./api");
+
+const httpServer = http.createServer(api);
+const socketServer = io(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
+const { listen } = require("./sockets");
+
 const PORT = 3000;
 
-server.listen(PORT);
+httpServer.listen(PORT);
 console.log(`Listening on ${PORT}`);
 
-let readyPlayerCount = 0;
-
-io.on("connection", (socket) => {
-  console.log("User connected with id:", socket.id);
-
-  socket.on("ready", () => {
-    console.log("Player ready", socket.id);
-    readyPlayerCount++;
-
-    if (readyPlayerCount % 2 === 0) {
-      io.emit("startGame", socket.id);
-    }
-  });
-
-  socket.on("paddleMove", (paddleData) => {
-    socket.broadcast.emit("paddleMove", paddleData);
-  });
-
-  socket.on("ballMove", (ballData) => {
-    socket.broadcast.emit("ballMove", ballData);
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  });
-});
+listen(socketServer);
