66b9bc6f598bd406349cee01f2e5d0f5a320331a
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-11-15 22:06:13 +0530

    add support for even number of players

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on("connection", (socket) => {
     console.log("Player ready", socket.id);
     readyPlayerCount++;
 
-    if (readyPlayerCount === 2) {
+    if (readyPlayerCount % 2 === 0) {
       io.emit("startGame", socket.id);
     }
   });
@@ -31,5 +31,9 @@ io.on("connection", (socket) => {
 
   socket.on("ballMove", (ballData) => {
     socket.broadcast.emit("ballMove", ballData);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`Client ${socket.id} disconnected: ${reason}`);
   });
 });
